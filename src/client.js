e08d252e3d4e8337f613823686c283a8558e470d
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-07-18 14:24:51 -0500

    Replace template with nextjs template

--- src/client.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "normalize.css";
-
-import { make as App } from "../lib/es6_global/src/App";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
