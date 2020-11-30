8a00b3875dc1a90732d57e9fec868c4bf73ff885
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-11-30 12:44:15 -0600

    Add meta tags for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,112 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>This Land I Stand</title>
+        <meta name="title" content="This Land I Stand" />
+        <meta
+          name="description"
+          content="A desktop app that tells you the traditional and/or contemporary lands you reside on. Powered by native-land.ca."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://thislandistand.app/" />
+        <meta property="og:title" content="This Land I Stand" />
+        <meta
+          property="og:description"
+          content="A desktop app that tells you the traditional and/or contemporary lands you reside on. Powered by native-land.ca."
+        />
+        <meta property="og:image" content="/logo.svg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://thislandistand.app/" />
+        <meta property="twitter:title" content="This Land I Stand" />
+        <meta
+          property="twitter:description"
+          content="A desktop app that tells you the traditional and/or contemporary lands you reside on. Powered by native-land.ca."
+        />
+        <meta property="twitter:image" content="/logo.svg" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
