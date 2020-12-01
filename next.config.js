22a15915f1b27ac1c756a06ad7940bed54acd4f5
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-11-30 21:00:42 -0600

    Try getting IP using serverless functions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  target: "severless",
   exportPathMap: async function () {
     return {
       "/": { page: "/" },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from "next/head";
 import { Main } from "../components/Main";
 import { Layout } from "../components/Layout";
+import { GetServerSidePropsContext } from "next";
+import requestIp from "request-ip";
+import geoip from "geoip-lite";
 
-export default function Home() {
+type Props = {
+  geo: any;
+};
+
+export default function Home(props: Props) {
+  console.log({ geo: props.geo });
+
   return (
     <>
       <Head>
@@ -13,4 +22,15 @@ export default function Home() {
       </Layout>
     </>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const clientIP = requestIp.getClientIp(context.req);
+  const geo = geoip.lookup(clientIP);
+
+  return {
+    props: {
+      geo,
+    },
+  };
 }
